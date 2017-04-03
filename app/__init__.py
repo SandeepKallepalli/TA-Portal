863946cf0424d5e082bc766c87357fa2e48009f2
pradeep <chandu555556555566666@gmail.com>
2017-04-03 22:38:50 +0530

    directory stucture uploaded

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+# import all things from flask
+from flask import *
+# Import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+from functools import wraps
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
+
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 200
+####################
+#add security so as to fin 
+####################
+
+
+
+#this creates all the tables in the data base 
+db.create_all()
+
