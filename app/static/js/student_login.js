e1729168c899ee99bc9137fbfc519c3ee2787441
hrshagithub <harrshavardhen.g@research.iiit.ac.in>
2017-04-12 02:49:47 +0530

    files added

---
+++ app/static/js/student_login.js
@@ -0,0 +1,38 @@
+$("#student_login_button").click(
+	function(){
+	var email = $("#student_login_email").val();		
+	callstudent_login_function();
+
+	alert("hello");	
+			}
+	
+	
+	
+	);
+
+
+var callstudent_login_function=function()
+			{
+		alert("hi enter");
+
+		var form1 = $('form[name=frm1]');	
+			 $.ajax({
+                url : 'http://127.0.0.1:8080/student/login',
+                method:"POST",
+		dataType : 'json',
+                data :  form1.serialize(),
+                success : function(responce){
+                alert("you are logged in");
+		alert(responce.name);
+				},
+                error : function(message){
+                alert("please fill valid details");
+		console.log(message);	
+				},
+                }
+        );
+
+
+			}
+
+
